b97b818c41aad4fd9e0d397ae11d12c0267c66b4
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2026-04-09 15:39:42 +0200

    update with properties

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const test = 
-`.page {
+const test = `.page {
     color:red !important !important;
     background:red
     font-size:10px
@@ -10,42 +9,54 @@ const test =
 }
 `;
 
+import cssData from "mdn-data/css/properties.json";
+const properties = Object.keys(cssData);
+
 function lint(input, settings) {
 	let issues = [];
 
-	function findImportant(input) {
-		const lines = input.split("\n");
+	const lines = input.split("\n");
 
-		lines.forEach((line, i) => {
-			// Detect any !important
-			if (/!important/.test(line))
-				issues.push({ position: { line: i + 1 }, message: `!important can be problematic`, severity: "low" });
+	lines.forEach((line, i) => {
+		const match = line.match(/^\s*([\w-]+)\s*:/d);
+		if (!match) return; // not a property line
+		const prop = match[1];
+		if (!properties.includes(prop)) {
+			console.log(`Invalid property: ${prop}`);
+			issues.push({ position: { line: i + 1 , from: match.indices[1][0], to: match.indices[1][1]}, message: `!important can be problematic`, severity: "low" });
+		}
+	});
 
-			// Detect multiple !important in the same line
+	lines.forEach((line, i) => {
+		// Detect any !important
+		if (/!important/.test(line))
+			issues.push({ position: { line: i + 1 }, message: `!important can be problematic`, severity: "low" });
+
+		// Detect multiple !important in the same line
+		/*
 			if (/!important.*!important/.test(line))
 				issues.push({
 					position: { line: i + 1 },
 					message: `don't use !important multiple times!`,
 					severity: "low",
 				});
+			*/
+		// CSS rules missing a colon (not last line before closing })
+		if (/^\s*[\w-]+\s*:[^;}\n]*$/.test(line))
+			issues.push({
+				position: { line: i + 1 },
+				message: `CSS rule missing semicolon`,
+				severity: "low",
+			});
 
-			// CSS rules missing a colon (not last line before closing })
-			if (/^\s*[\w-]+\s*:[^;}\n]*$/.test(line))
-				issues.push({
-					position: { line: i + 1 },
-					message: `CSS rule missing semicolon`,
-					severity: "low",
-				});
-
-			// CSS rules missing a value but have colon
-			if (/^\s*[\w-]+\s*:\s*(?=;?$)/.test(line))
-				issues.push({
-					position: { line: i + 1 },
-					message: `CSS rule missing value`,
-					severity: "low",
-				});
-		});
-	}
+		// CSS rules missing a value but have colon
+		if (/^\s*[\w-]+\s*:\s*(?=;?$)/.test(line))
+			issues.push({
+				position: { line: i + 1 },
+				message: `CSS rule missing value`,
+				severity: "low",
+			});
+	});
 
 	findImportant(input);
 
